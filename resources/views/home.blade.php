f6559d67442e392ccb13d3c553a237f5246be015
aep <aepsaprul18@gmail.com>
2021-05-04 09:52:57 +0700

    update home (ketika register status lamaran = 0)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,7 +85,81 @@
                     </form>
                     {{-- end form persyaratan  --}}
                 @else
-                    @if ($lamaran->status_lamaran == 1)
+                    @if ($lamaran->status_lamaran == 0)
+                        {{-- form persyaratan  --}}
+                        <h3 class="text-uppercase font-weight-bold p-2 text-center selamatdatang">selamat datang</h3>
+                        <p class="text-center text-capitalize font-weight-bold">silahkan isi formulir dengan lengkap dan benar</p>
+                        <form action="{{ route('home.persyaratan.update') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="master_jabatan_id">Posisi yg dilamar</label>
+                                        <select name="master_jabatan_id" id="master_jabatan_id" class="form-control">
+                                            <option value="">--Pilih Posisi--</option>
+                                            @foreach ($lokers as $loker)  
+                                                <option value="{{ $loker->master_jabatan_id }}">{{ $loker->masterJabatan->nama_jabatan }}</option>    
+                                            @endforeach
+                                        </select>  
+                                    </div>  
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="surat_lamaran">Scan Surat Lamaran (JPG/PNG max size 2MB)</label>
+                                        <input type="file" name="surat_lamaran" class="form-control @error('surat_lamaran') is-invalid @enderror" id="surat_lamaran" autofocus value="{{ old('surat_lamaran') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="curriculum_vitae">Scan Curriculum Vitae (JPG/PNG max size 2MB)</label>
+                                        <input type="file" name="curriculum_vitae" class="form-control @error('curriculum_vitae') is-invalid @enderror" id="curriculum_vitae" autofocus value="{{ old('curriculum_vitae') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="ijazah">Scan Ijazah (JPG/PNG max size 2MB)</label>
+                                        <input type="file" name="ijazah" class="form-control @error('ijazah') is-invalid @enderror" id="ijazah" autofocus value="{{ old('ijazah') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="transkip_nilai">Scan Transkip Nilai (JPG/PNG max size 2MB)</label>
+                                        <input type="file" name="transkip_nilai" class="form-control @error('transkip_nilai') is-invalid @enderror" id="transkip_nilai" autofocus value="{{ old('transkip_nilai') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="foto">Foto 3x4</label>
+                                        <input type="file" name="foto" class="form-control @error('foto') is-invalid @enderror" id="foto" autofocus value="{{ old('foto') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="kartu_keluarga">Scan Kartu Keluarga (JPG/PNG max size 2MB)</label>
+                                        <input type="file" name="kartu_keluarga" class="form-control @error('kartu_keluarga') is-invalid @enderror" id="kartu_keluarga" autofocus value="{{ old('kartu_keluarga') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="ktp">Scan KTP (JPG/PNG max size 2MB)</label>
+                                        <input type="file" name="ktp" class="form-control @error('ktp') is-invalid @enderror" id="ktp" autofocus value="{{ old('ktp') }}">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <hr class="border-secondary">
+                                    </div> 
+                                </div> 
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <button class="btn btn-primary btn-block">Kirim</button>
+                                    </div> 
+                                </div> 
+                            </div>
+                        </form>
+                        {{-- end form persyaratan  --}}
+                    @elseif ($lamaran->status_lamaran == 1)
                         <h3 class="text-uppercase font-weight-bold p-2 text-center selamatdatang">terima kasih</h3>
                         <p class="text-center text-capitalize font-weight-bold">berkas sedang kami proses...</p>
                     @elseif ($lamaran->status_lamaran == 7)
